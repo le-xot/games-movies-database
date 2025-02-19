2a3d9b9ca133c879d4e8c8efbc3013ced68977b4
le-xot <lexot.dev@gmail.com>
2025-02-19 11:20:27 +0500

    fix(backend): direct usage process.env

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,20 +1,19 @@
 const { PrismaClient } = require('@prisma/client')
 require('dotenv').config()
 const process = require('node:process')
-const {env} = require("../src/utils/enviroments");
 
 const prisma = new PrismaClient()
 
 async function seed() {
   await prisma.user.upsert(
     {
-      where: { id: env.TWITCH_ADMIN_ID },
+      where: { id: process.env.TWITCH_ADMIN_ID },
       update: {
         role: 'ADMIN',
       },
       create: {
-        id: env.TWITCH_ADMIN_ID,
-        login: env.TWITCH_ADMIN_LOGIN,
+        id: process.env.TWITCH_ADMIN_ID,
+        login: process.env.TWITCH_ADMIN_LOGIN,
         role: 'ADMIN',
       },
     },

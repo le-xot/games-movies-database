a2bdf7c11ffbb5fbf1d2b0f4e13ae649b224afd4
le-xot <lexot.dev@gmail.com>
2025-05-08 14:05:11 +0500

    hotfix

--- backend/src/modules/suggestion/suggestion.service.ts
+++ backend/src/modules/suggestion/suggestion.service.ts
@@ -114,6 +114,8 @@ export class SuggestionService {
   }
 
   private async fetchKinopoisk(id: number): Promise<CreateSuggestion> {
+    console.log(id)
+
     const response = await fetch(
       `https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`,
       {
@@ -123,6 +125,8 @@ export class SuggestionService {
         },
       },
     )
+
+    console.log(response)
 
     if (!response.ok) {
       throw new BadRequestException('Не удалось получить данные из API Кинопоиска')
@@ -174,12 +178,15 @@ export class SuggestionService {
     const patterns = {
       shikimori: /shikimori\.one\/animes\/[a-z]?(\d+)/,
       kinopoisk: /kinopoisk\.ru\/(film|series)\/(\d+)/,
-
     }
     for (const [service, pattern] of Object.entries(patterns)) {
       const match = link.match(pattern)
-      if (match && match[1]) {
-        return { service, id: Number(match[1]) }
+      if (match) {
+        if (service === 'kinopoisk') {
+          return { service, id: Number(match[2]) }
+        } else {
+          return { service, id: Number(match[1]) }
+        }
       }
     }
     throw new BadRequestException('Неверный или неподдерживаемый формат ссылки')

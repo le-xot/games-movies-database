872ada088f3f09b335b209726edf8889829a3210
le-xot <lexot.dev@gmail.com>
2025-05-11 16:55:24 +0500

    feat(backend): new prisma schema

---
+++ backend/prisma/migrations/20250511115329_/migration.sql
@@ -0,0 +1,70 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "RecordStatus" AS ENUM ('QUEUE', 'PROGRESS', 'DROP', 'UNFINISHED', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "RecordType" AS ENUM ('HANDWRITTEN', 'SUGGESTION', 'AUCTION', 'ORDER');
+
+-- CreateEnum
+CREATE TYPE "RecordGenre" AS ENUM ('GAME', 'MOVIE', 'ANIME', 'CARTOON', 'SERIES');
+
+-- CreateEnum
+CREATE TYPE "RecordGrade" AS ENUM ('DISLIKE', 'BEER', 'LIKE', 'RECOMMEND');
+
+-- CreateEnum
+CREATE TYPE "LimitType" AS ENUM ('SUGGESTION');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "role" "UserRole" DEFAULT 'USER',
+    "profileImageUrl" TEXT,
+    "color" TEXT NOT NULL DEFAULT '#333333',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "records" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "posterUrl" TEXT,
+    "status" "RecordStatus" NOT NULL DEFAULT 'QUEUE',
+    "type" "RecordType" NOT NULL DEFAULT 'HANDWRITTEN',
+    "genre" "RecordGenre",
+    "grade" "RecordGrade",
+    "episode" TEXT,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "records_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "limits" (
+    "name" "LimitType" NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 5
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_login_key" ON "users"("login");
+
+-- CreateIndex
+CREATE INDEX "users_id_login_idx" ON "users"("id", "login");
+
+-- CreateIndex
+CREATE INDEX "records_title_idx" ON "records"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "limits_name_key" ON "limits"("name");
+
+-- AddForeignKey
+ALTER TABLE "records" ADD CONSTRAINT "records_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -5,29 +5,29 @@ const process = require('node:process')
 const prisma = new PrismaClient()
 
 async function seed() {
-  await prisma.user.upsert(
-    {
-      where: { id: process.env.TWITCH_ADMIN_ID },
-      update: {
-        role: 'ADMIN',
-      },
-      create: {
-        id: process.env.TWITCH_ADMIN_ID,
-        login: process.env.TWITCH_ADMIN_LOGIN,
-        role: 'ADMIN',
-      },
-    },
-  )
-  await prisma.limit.upsert(
-    {
-      where: { name: 'SUGGESTION' },
-      update: {},
-      create: {
-        name: 'SUGGESTION',
-        quantity: 3,
-      },
-    },
-  )
+  // await prisma.user.upsert(
+  //   {
+  //     where: { id: process.env.TWITCH_ADMIN_ID },
+  //     update: {
+  //       role: 'ADMIN',
+  //     },
+  //     create: {
+  //       id: process.env.TWITCH_ADMIN_ID,
+  //       login: process.env.TWITCH_ADMIN_LOGIN,
+  //       role: 'ADMIN',
+  //     },
+  //   },
+  // )
+  // await prisma.limit.upsert(
+  //   {
+  //     where: { name: 'SUGGESTION' },
+  //     update: {},
+  //     create: {
+  //       name: 'SUGGESTION',
+  //       quantity: 3,
+  //     },
+  //   },
+  // )
 }
 
 seed()

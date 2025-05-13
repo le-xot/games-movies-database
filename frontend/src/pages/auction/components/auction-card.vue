b4c1a5a46202ac0cf721573b1cbdf907e2985c1f
le-xot <lexot.dev@gmail.com>
2025-05-14 01:53:04 +0500

    upd(frontend): auction card

--- frontend/src/pages/auction/components/auction-card.vue
+++ frontend/src/pages/auction/components/auction-card.vue
@@ -1,42 +1,20 @@
 <script setup lang="ts">
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { toast } from '@/components/ui/toast'
 import { useUser } from '@/composables/use-user'
 import { RecordEntity } from '@/lib/api.ts'
 import { storeToRefs } from 'pinia'
 import { ref } from 'vue'
-import { useAuctions } from '../composables/use-autions'
 
 defineProps<{ items: RecordEntity[] }>()
 
 const { isAdmin } = storeToRefs(useUser())
-
-const auctions = useAuctions()
 
 const isDialogOpen = ref(false)
 
 isDialogOpen.value = false
 
 defineExpose({ isDialogOpen })
-
-async function handleApproveSuggestion(id: number) {
-  try {
-    await auctions.approveAuction(id)
-    toast({
-      title: 'Успешно',
-      description: 'Совет одобрен',
-      variant: 'default',
-    })
-  } catch {
-    toast({
-      title: 'Ошибка',
-      description: auctions.error || 'Не удалось одобрить совет',
-      variant: 'destructive',
-    })
-  }
-}
 </script>
 
 <template>
@@ -83,15 +61,6 @@ async function handleApproveSuggestion(id: number) {
                 <div class="text-base text-white font-medium">
                   {{ item.user.login }}
                 </div>
-              </div>
-              <div v-if="isAdmin" class="flex justify-end w-full mt-auto gap-2">
-                <Button
-                  size="sm"
-                  class="text-sm"
-                  @click="handleApproveSuggestion(item.id)"
-                >
-                  В базу
-                </Button>
               </div>
             </CardFooter>
           </div>

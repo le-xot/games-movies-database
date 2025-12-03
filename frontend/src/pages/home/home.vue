5ec23a2acecc6ad982cb1b0b37b46d862d570ff4
le-xot <lexot.dev@gmail.com>
2025-12-03 06:55:40 +0500

    chore(frontend): fix blinking links

--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { ROUTER_PATHS } from '@/lib/router/router-paths'
+import { useTitle } from '@vueuse/core'
 import { ComputerIcon, LibraryBigIcon } from 'lucide-vue-next'
+import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 import {
   DiscordIcon,
   GitHubIcon,
@@ -8,84 +12,111 @@ import {
   TwitchIcon,
   YouTubeIcon,
 } from 'vue3-simple-icons'
+
+const title = useTitle()
+onMounted(() => title.value = 'Лешот.ру')
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center p-8">
-    <div class="w-full max-w-5xl">
-      <div class="grid grid-cols-3 gap-6" style="grid-template-rows: repeat(4, 1fr)">
-        <a href="/" class="col-start-1 col-span-2 row-start-1 row-span-2 rounded-3xl overflow-hidden">
+    <div class="w-full max-w-4xl">
+      <div class="grid grid-cols-3 gap-4" style="grid-template-rows: repeat(4, 1fr)">
+        <RouterLink :to="ROUTER_PATHS.home" class="col-start-1 col-span-2 row-start-1 row-span-2 rounded-2xl overflow-hidden">
           <img src="/images/lexot.webp" alt="Лешот" class="w-full h-full object-cover">
-        </a>
-        <a
-          href="/db/games" class="col-start-3 row-start-1 rounded-3xl flex flex-col justify-center p-8 text-white"
+        </RouterLink>
+        <RouterLink
+          :to="ROUTER_PATHS.dbGames" class="col-start-3 row-start-1 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #166534CC"
         >
-          <LibraryBigIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Кладовка</h3>
-          <p class="mt-2 opacity-90">База данных игр и кинолент</p>
-        </a>
-        <a
-          href="/pc" class="col-start-3 row-start-2 rounded-3xl flex flex-col justify-center p-8 text-white"
+          <div class="flex items-center mb-3">
+            <LibraryBigIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">
+              Кладовка
+            </h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">
+            База данных игр и кинолент
+          </p>
+        </RouterLink>
+        <RouterLink
+          :to="ROUTER_PATHS.pc" class="col-start-3 row-start-2 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #925f0dCC"
         >
-          <ComputerIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Железо</h3>
-          <p class="mt-2 opacity-90">Конфигурация компьютера и периферия</p>
-        </a>
+          <div class="flex items-center mb-3">
+            <ComputerIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">
+              Железо
+            </h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">
+            Конфигурация компьютера и периферия
+          </p>
+        </RouterLink>
         <a
-          href="https://steamcommunity.com/id/le_xot" target="_blank" rel="noopener"
-          class="row-start-3 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://steamcommunity.com/id/le_xot" target="_blank"
+          class="row-start-3 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #000000CC"
         >
-          <SteamIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Steam</h3>
-          <p class="mt-2 opacity-90">Мой личный архив пиксельных приключений</p>
+          <div class="flex items-center mb-3">
+            <SteamIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">Steam</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Мой личный архив пиксельных приключений</p>
         </a>
         <a
-          href="https://github.com/le-xot" target="_blank" rel="noopener"
-          class="row-start-3 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://github.com/le-xot" target="_blank"
+          class="row-start-3 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #181717CC"
         >
-          <GitHubIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">GitHub</h3>
-          <p class="mt-2 opacity-90">Код, который иногда работает</p>
+          <div class="flex items-center mb-3">
+            <GitHubIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">GitHub</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Код, который иногда работает</p>
         </a>
         <a
-          href="https://t.me/le_xot" target="_blank" rel="noopener"
-          class="row-start-3 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://t.me/le_xot" target="_blank"
+          class="row-start-3 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #26A5E4CC"
         >
-          <TelegramIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Telegram</h3>
-          <p class="mt-2 opacity-90">Самый быстрый способ достучаться</p>
+          <div class="flex items-center mb-3">
+            <TelegramIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">Telegram</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Самый быстрый способ достучаться</p>
         </a>
         <a
-          href="https://discord.gg/zgZHHnkkP5" target="_blank" rel="noopener"
-          class="row-start-4 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://twir.app/s/lexotovo" target="_blank"
+          class="row-start-4 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #5865F2CC"
         >
-          <DiscordIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Discord</h3>
-          <p class="mt-2 opacity-90">Деревня Лешотово</p>
+          <div class="flex items-center mb-3">
+            <DiscordIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">Discord</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Деревня Лешотово</p>
         </a>
         <a
-          href="https://www.twitch.tv/etonelexot" target="_blank" rel="noopener"
-          class="row-start-4 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://www.twitch.tv/etonelexot" target="_blank"
+          class="row-start-4 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #9146FFCC"
         >
-          <TwitchIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">Twitch</h3>
-          <p class="mt-2 opacity-90">Стримлю, когда не ленюсь</p>
+          <div class="flex items-center mb-3">
+            <TwitchIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">Twitch</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Стримлю, когда не ленюсь</p>
         </a>
         <a
-          href="https://www.youtube.com/@le_xot" target="_blank" rel="noopener"
-          class="row-start-4 rounded-3xl flex flex-col justify-center p-8 text-white"
+          href="https://www.youtube.com/@le_xot" target="_blank"
+          class="row-start-4 rounded-2xl flex flex-col justify-center p-6 text-white"
           style="background-color: #FF0000CC"
         >
-          <YouTubeIcon class="w-12 h-12 mb-4" />
-          <h3 class="text-2xl font-bold">YouTube</h3>
-          <p class="mt-2 opacity-90">Канал, застрявший в золотой эпохе летсплеев</p>
+          <div class="flex items-center mb-3">
+            <YouTubeIcon class="w-10 h-10 mr-3" />
+            <h3 class="text-xl font-bold">YouTube</h3>
+          </div>
+          <p class="mt-1 opacity-90 text-sm">Канал, застрявший в золотой эпохе летсплеев</p>
         </a>
       </div>
     </div>

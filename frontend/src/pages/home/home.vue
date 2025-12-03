f10dff71ecf2bdeee895443bc4a33278659adfde
le-xot <lexot.dev@gmail.com>
2025-12-03 07:59:14 +0500

    chore(frontend): add mobile adaptive

--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -11,95 +11,95 @@ onMounted(() => title.value = 'Лешот.ру')
 <template>
   <div class="min-h-screen flex items-center justify-center p-8">
     <div class="w-full max-w-4xl">
-      <div class="grid grid-cols-3 gap-4" style="grid-template-rows: repeat(4, 1fr)">
-        <RouterLink :to="ROUTER_PATHS.home" class="col-start-1 col-span-2 row-start-1 row-span-2 rounded-5xl overflow-hidden">
-          <img src="/images/lexot.webp" alt="Лешот" class="w-full h-full object-cover">
+      <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+        <RouterLink :to="ROUTER_PATHS.home" class="hidden md:block col-span-2 row-span-2 rounded-5xl overflow-hidden">
+          <img src="/images/lexot.webp" alt="Лешот" class="object-cover">
         </RouterLink>
         <RouterLink
-          :to="ROUTER_PATHS.dbGames" class="col-start-3 row-start-1 outline rounded-5xl flex flex-col justify-between p-6 text-white"
+          :to="ROUTER_PATHS.dbGames" class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #166534CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">
+            <h3 class="text-xl md:text-5xl font-bold">
               Кладовка
             </h3>
           </div>
-          <p class="opacity-90 text-3xl">
+          <p class="opacity-90 text-xs md:text-3xl">
             База данных игр и кинолент
           </p>
         </RouterLink>
         <RouterLink
-          :to="ROUTER_PATHS.pc" class="col-start-3 row-start-2 outline rounded-5xl flex flex-col justify-between p-6 text-white"
+          :to="ROUTER_PATHS.pc" class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #925f0dCC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">
+            <h3 class="text-xl md:text-5xl font-bold">
               Железяки
             </h3>
           </div>
-          <p class="opacity-90 text-3xl">
+          <p class="opacity-90 text-xs md:text-3xl">
             Конфигурация компьютера и периферия
           </p>
         </RouterLink>
         <a
           href="https://steamcommunity.com/id/le_xot" target="_blank"
-          class="row-start-3 rounded-5xl flex flex-col justify-between p-6 outline text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #000000CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">Steam</h3>
+            <h3 class="text-xl md:text-5xl font-bold">Steam</h3>
           </div>
-          <p class="opacity-90 text-3xl">Мой архив пиксельных приключений</p>
+          <p class="opacity-90 text-xs md:text-3xl">Архив пиксельных приключений</p>
         </a>
         <a
           href="https://github.com/le-xot" target="_blank"
-          class="row-start-3 rounded-5xl flex flex-col justify-between outline p-6 text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #181717CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">GitHub</h3>
+            <h3 class="text-xl md:text-5xl font-bold">GitHub</h3>
           </div>
-          <p class="opacity-90 text-3xl">Код, который иногда работает</p>
+          <p class="opacity-90 text-xs md:text-3xl">Код, который иногда работает</p>
         </a>
         <a
           href="https://t.me/le_xot" target="_blank"
-          class="row-start-3 rounded-5xl flex outline flex-col justify-between p-6 text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #26A5E4CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">Telegram</h3>
+            <h3 class="text-xl md:text-5xl font-bold">Telegram</h3>
           </div>
-          <p class="opacity-90 text-3xl">Самый быстрый способ достучаться</p>
+          <p class="opacity-90 text-xs md:text-3xl">Быстрый способ достучаться</p>
         </a>
         <a
           href="https://twir.app/s/lexotovo" target="_blank"
-          class="row-start-4 rounded-5xl flex outline flex-col justify-between p-6 text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #5865F2CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">Discord</h3>
+            <h3 class="text-xl md:text-5xl font-bold">Discord</h3>
           </div>
-          <p class="opacity-90 text-3xl">Деревня "Лешотово"</p>
+          <p class="opacity-90 text-xs md:text-3xl">Деревня "Лешотово"</p>
         </a>
         <a
           href="https://www.twitch.tv/etonelexot" target="_blank"
-          class="row-start-4 rounded-5xl flex outline flex-col justify-between p-6 text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #9146FFCC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">Twitch</h3>
+            <h3 class="text-xl md:text-5xl font-bold">Twitch</h3>
           </div>
-          <p class="opacity-90 text-3xl">Стримлю, когда не ленюсь</p>
+          <p class="opacity-90 text-xs md:text-3xl">Стримлю, когда не ленюсь</p>
         </a>
         <a
           href="https://www.youtube.com/@le_xot" target="_blank"
-          class="row-start-4 rounded-5xl flex outline flex-col justify-between p-6 text-white"
+          class="outline rounded-5xl flex flex-col justify-between p-4 text-white h-[20vh] md:h-auto"
           style="background-color: #FF0000CC"
         >
           <div class="flex justify-between">
-            <h3 class="text-5xl font-bold">YouTube</h3>
+            <h3 class="text-xl md:text-5xl font-bold">YouTube</h3>
           </div>
-          <p class="opacity-90 text-3xl">Канал, прямиком из эпохи летсплеев</p>
+          <p class="opacity-90 text-xs md:text-3xl">Канал, прямиком из эпохи летсплеев</p>
         </a>
       </div>
     </div>

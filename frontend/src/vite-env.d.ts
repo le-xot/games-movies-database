100d99386b4a1b2d1b4433e5b62373374ff217c2
Alexander <satontworldwide@gmail.com>
2026-02-01 14:30:57 +0300

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="vite/client" />
-declare module '*.vue';
+declare module '*.vue' {
+	import type { DefineComponent } from 'vue'
+	const component: DefineComponent<object, object, unknown>
+	export default component
+}

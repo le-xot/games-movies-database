5567771ad57471f69422c079333e274c72d17cf3
le-xot <lexot.dev@gmail.com>
2026-02-21 15:11:37 +0500

    fix(frontend): change watchlink to kinohub

--- frontend/src/lib/utils/generate-watch-link.ts
+++ frontend/src/lib/utils/generate-watch-link.ts
@@ -2,19 +2,19 @@ export function generateWatchLink(originalLink: string): string | null {
   const shikimoriMatch = originalLink.match(/shikimori\.one\/animes\/([a-z]?\d+)/)
   if (shikimoriMatch) {
     const movieId = shikimoriMatch[1]
-    return `https://reyohoho.github.io/reyohoho/#shiki${movieId}`
+    return `https://kinobox.in/shikimori/${movieId}`
   }
 
   const kinopoiskMatch = originalLink.match(/kinopoisk\.ru\/(film|series)\/(\d+)/)
   if (kinopoiskMatch) {
     const movieId = kinopoiskMatch[2]
-    return `https://reyohoho.github.io/reyohoho/#${movieId}`
+    return `https://kinobox.in/movie/${movieId}`
   }
 
   const imdbMatch = originalLink.match(/imdb\.com\/title\/(tt\d+)/)
   if (imdbMatch) {
     const movieId = imdbMatch[1]
-    return `https://reyohoho.github.io/reyohoho/#imdb=${movieId}`
+    return `https://kinobox.in/movie/${movieId}`
   }
 
   const igdbMatch = originalLink.match(/igdb\.com\/games\/([^/]+)/)

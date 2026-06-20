a23a9628de6c9dc64bacf7ab484d5b77cf8ab424
le-xot <lexot.dev@gmail.com>
2026-06-21 00:14:02 +0500

    fix: remove duplicate TwitchService registration from AuthModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -2,13 +2,12 @@ import { Global, Module } from '@nestjs/common'
 import { AuthController } from '@/modules/auth/auth.controller'
 import { AuthService } from '@/modules/auth/auth.service'
 import { KickModule } from '@/modules/kick/kick.module'
-import { TwitchService } from '@/modules/twitch/twitch.service'
 import { UserModule } from '@/modules/user/user.module'
 
 @Global()
 @Module({
   imports: [UserModule, KickModule],
-  providers: [AuthService, TwitchService],
+  providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
 })

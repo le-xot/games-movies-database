8c535be8f29202de4f2c4a7a0c05f0a288fa0b67
le-xot <lexot.dev@gmail.com>
2026-06-21 00:15:03 +0500

    refactor: make KickModule global, remove redundant import from AuthModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common'
 import { AuthController } from '@/modules/auth/auth.controller'
 import { AuthService } from '@/modules/auth/auth.service'
-import { KickModule } from '@/modules/kick/kick.module'
 import { UserModule } from '@/modules/user/user.module'
 
 @Global()
 @Module({
-  imports: [UserModule, KickModule],
+  imports: [UserModule],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],

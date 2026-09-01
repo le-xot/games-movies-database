36ccf6844a5ac0d0a3134506889b7e0ab702d728
le-xot <lexot.dev@gmail.com>
2026-09-02 03:30:06 +0500

    feat: remove dotenv import from prisma configuration

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'prisma/config'
 
-// eslint-disable-next-line import/no-unassigned-import
-import 'dotenv/config'
-
 export default defineConfig({
   datasource: {
     url: process.env.DATASOURCE_URL!,

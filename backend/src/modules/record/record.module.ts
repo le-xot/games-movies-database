7018e706d5fe0c241500eab5ca0e0e8ec8e9f439
le-xot <lexot.dev@gmail.com>
2026-09-15 22:19:44 +0500

    refactor(backend): port record, suggestion and queue repositories to drizzle

--- backend/src/modules/record/record.module.ts
+++ backend/src/modules/record/record.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common'
-import { PrismaModule } from '@/database/prisma.module'
+import { DrizzleModule } from '@/database/drizzle.module'
 import { ImgModule } from '@/modules/img/img.module'
 import { RecordController } from '@/modules/record/record.controller'
 import { RecordService } from '@/modules/record/record.service'
-import { PrismaRecordRepository } from '@/modules/record/repositories/prisma-record.repository'
 import { RecordRepository } from '@/modules/record/repositories/record.repository'
 import { RecordsProvidersModule } from '@/modules/records-providers/records-providers.module'
 import { UserModule } from '@/modules/user/user.module'
 import { WebsocketModule } from '@/modules/websocket/websocket.module'
+import { DrizzleRecordRepository } from './repositories/drizzle-record.repository'
 
 @Module({
-  imports: [PrismaModule, UserModule, RecordsProvidersModule, WebsocketModule, ImgModule],
-  providers: [RecordService, { provide: RecordRepository, useClass: PrismaRecordRepository }],
+  imports: [DrizzleModule, UserModule, RecordsProvidersModule, WebsocketModule, ImgModule],
+  providers: [RecordService, { provide: RecordRepository, useClass: DrizzleRecordRepository }],
   controllers: [RecordController],
   exports: [RecordRepository],
 })
--- backend/src/modules/suggestion/suggestion.module.ts
+++ backend/src/modules/suggestion/suggestion.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
-import { PrismaModule } from '@/database/prisma.module'
+import { DrizzleModule } from '@/database/drizzle.module'
 import { RecordsProvidersModule } from '@/modules/records-providers/records-providers.module'
-import { PrismaSuggestionRepository } from '@/modules/suggestion/repositories/prisma-suggestion.repository'
+import { DrizzleSuggestionRepository } from '@/modules/suggestion/repositories/drizzle-suggestion.repository'
 import { SuggestionRepository } from '@/modules/suggestion/repositories/suggestion.repository'
 import { SuggestionController } from '@/modules/suggestion/suggestion.controller'
 import { SuggestionService } from '@/modules/suggestion/suggestion.service'
@@ -9,10 +9,10 @@ import { UserModule } from '@/modules/user/user.module'
 import { WebsocketModule } from '@/modules/websocket/websocket.module'
 
 @Module({
-  imports: [PrismaModule, UserModule, RecordsProvidersModule, WebsocketModule],
+  imports: [DrizzleModule, UserModule, RecordsProvidersModule, WebsocketModule],
   providers: [
     SuggestionService,
-    { provide: SuggestionRepository, useClass: PrismaSuggestionRepository },
+    { provide: SuggestionRepository, useClass: DrizzleSuggestionRepository },
   ],
   controllers: [SuggestionController],
   exports: [SuggestionService],
